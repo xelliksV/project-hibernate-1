0522dc09572dfa16d507394391f6275b2a999779
Vladislav <xelliks666@gmail.com>
2025-05-24 01:24:47 +0700

    Fixed problem with ID generation

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -8,7 +8,7 @@ import java.util.Date;
 @Table(name = "player", schema = "rpg")
 public class Player {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue()
     private Long id;
     @Column(name = "name", nullable = false, length = 12)
     private String name;
